e0195ec655d5bfe285ab1d52fa6d67149e2e2bd9
iarley <7iarley7@gmail.com>
2025-01-16 23:59:40 -0300

    Cria conexao com o banco de dados

---
+++ Database/Connection/MongoDb.js
@@ -0,0 +1,25 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../Env/Mongo/.env') });
+const mongoose = require("mongoose");
+
+// Monta a URL dinamicamente usando variáveis de ambiente
+const mongoUser     = process.env.MONGO_INITDB_ROOT_USERNAME;
+const mongoPassword = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const mongoHost     = process.env.MONGO_HOST || 'localhost';
+const mongoPort     = process.env.MONGO_PORT || 27017;
+const mongoDatabase = process.env.MONGO_INITDB_DATABASE;
+
+const mongoURL = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDatabase}?authSource=admin`;
+
+mongoose.connect(mongoURL);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", () => {
+    console.log("======================");
+    console.log("Connected to database");
+});
+
+module.exports = db;
