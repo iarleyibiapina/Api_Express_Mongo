524132e9ff41347145109ed80c37666c6d5456d7
iarley <7iarley7@gmail.com>
2025-01-17 00:22:05 -0300

    criando container node

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const app  = require("express")();
 const express = require("express");
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './Env/Node/.env') });
 
 // CONEXAO COM O BANCO
-const db     = require("./Database/Connection/MongoDb.js")
-const Router = require("./Routes/Router.js");
-const FILTER = require("./Http/Filters/Filter.js");
+const db     = require("./app/Database/Connection/MongoDb.js")
+const Router = require("./app/Routes/Router.js");
+const FILTER = require("./app/Http/Filters/Filter.js");
 // 
 app.use(express.json()); // parsea para json
 
@@ -13,7 +15,7 @@ app.use('/api', Router);
 app.use(FILTER);
 
 // HTTP
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("======================");
     console.log(`Servidor rodando - localhost:${PORT}`);
